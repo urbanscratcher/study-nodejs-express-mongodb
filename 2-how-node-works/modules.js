eb798cce4e11f53058c6085efaa50b105a3c7c8c
Joun <urbanscratcher@gmail.com>
2023-06-30 22:15:52 +0900

    .

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -7,5 +7,11 @@ const calc1 = new C();
 console.log(calc1.add(2, 4));
 
 // exports
-const { add, multiply, divide } = require("./test-module-2");
+const { add, multiply } = require("./test-module-2");
 console.log(add(2, 4));
+console.log(multiply(2, 4));
+
+// caching
+require("./test-module-3")();
+require("./test-module-3")();
+require("./test-module-3")();
