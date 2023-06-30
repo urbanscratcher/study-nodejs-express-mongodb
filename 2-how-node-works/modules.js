dc95860b4bc3120a3504adafad0ebd99d66bf075
Joun <urbanscratcher@gmail.com>
2023-06-30 22:05:06 +0900

    module.exports, exports

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -1,7 +1,7 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
 
-// moudle.exports
+// module.exports
 const C = require("./test-module-1");
 const calc1 = new C();
 console.log(calc1.add(2, 4));
