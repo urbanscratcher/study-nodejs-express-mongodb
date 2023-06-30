b760842504d71afd5771b982e1dc01c8c15fd920
Joun <urbanscratcher@gmail.com>
2023-06-30 22:14:53 +0900

    module.exports, exports

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -7,5 +7,5 @@ const calc1 = new C();
 console.log(calc1.add(2, 4));
 
 // exports
-const calc2 = require("./test-module-2");
-console.log(calc2.add(2, 4));
+const { add, multiply, divide } = require("./test-module-2");
+console.log(add(2, 4));
