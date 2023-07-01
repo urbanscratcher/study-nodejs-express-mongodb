6d5da5bce3e8a101f32865df2c35e0e37bf7e9e7
Joun <urbanscratcher@gmail.com>
2023-07-01 19:33:48 +0900

    req params. get one

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -30,7 +30,6 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -48,6 +47,26 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+// params are required by default. making optional /:param?
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
 });
 
 const port = 3000;
