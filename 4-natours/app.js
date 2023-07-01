e91348974a1b0d3f4fb2490fbb13d898914fceb6
Joun <urbanscratcher@gmail.com>
2023-07-02 01:24:46 +0900

    profiling

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,8 +7,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
 
 // load static files
 app.use(express.static(`${__dirname}/public`));
