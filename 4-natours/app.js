55d6701f292d9d48edd2638d4eb2fa64516c2351
Joun <urbanscratcher@gmail.com>
2023-07-02 00:51:33 +0900

    router params

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -9,6 +9,11 @@ const {
 
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+  next();
+});
+
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
